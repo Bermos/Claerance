4d7bbb0cb8e7ac16c38fb96d32bfb77ffca5e5f7
Bermos <accounts@machivenyika.ch>
2020-06-17 15:10:35 +0200

    Updates main package to setup new database correctly

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"Claerance/internal/database"
 	"Claerance/internal/server"
 	"Claerance/internal/users"
 )
 
 func main() {
+	database.SetDriver("sqlite3")
+	database.SetURI("test.db")
+
 	users.Setup()
+
+	server.InitSessionStore()
 	server.Start(1401)
 }

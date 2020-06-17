e37f260281011069b31760b1d781a8fed3ebe100
Bermos <accounts@machivenyika.ch>
2020-06-17 15:05:46 +0200

    Ads login functionality to frontend

--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,14 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  private sessionsUrl = 'api/v1/session';
+  username: string;
+  password: string;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    console.log('Hi');
-  }
+    if (!this.username || !this.password) {
+      console.log('Username or password missing');
+      return;
+    }
 
+    this.http.post(this.sessionsUrl, {
+      username: this.username,
+      password: this.password
+    }).subscribe(() => this.router.navigate(['/dashboard']));
+  }
 }

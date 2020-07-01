5cc74f677e42199e0a6bb1542c3e522f99d59992
Bermos <accounts@machivenyika.ch>
2020-07-01 18:45:46 +0200

    Adds routing module for frontend

---
+++ web/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+
+/*
+Routing list, can't use /api route for anything since the go server will catch it an use for itself.
+ */
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

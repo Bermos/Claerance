4ccf2579f169eb7421070b1a80b0103e36a58ce0
Matthew Machivenyika <accounts@machivenyika.ch>
2021-12-15 16:45:44 +0000

    Add automation job

---
+++ .space.kts
@@ -0,0 +1,22 @@
+job("Build and push Docker") {
+     startOn {
+        gitPush { 
+        	branchFilter {
+                +"refs/heads/master"
+            }
+        }
+     }
+     
+    docker {
+        build {
+            context = "docker"
+            file = "./docker/Dockerfile"
+            labels["vendor"] = "Bermos Inc."
+            args["HTTP_PROXY"] = "http://10.20.30.1:123"
+        }
+
+        push("bermos.registry.jetbrains.space/p/claerance/containers/claerance") {
+            tags("version1.0." + api.executionNumber())
+        }
+    }
+}

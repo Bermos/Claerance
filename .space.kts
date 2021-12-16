e525d94826a202e42c752c1dc7eafba09bc8e01f
Matthew Machivenyika <matthew.machivenyika@fis-he.ch>
2021-12-16 07:18:24 +0100

    Experiment with versioning in CI

--- .space.kts
+++ .space.kts
@@ -12,12 +12,12 @@ job("Build and push Docker") {
             context = "."
             file = "./docker/Dockerfile"
             labels["vendor"] = "Bermos Inc."
-            labels["version"] = "0.1"
+            labels["version"] = "0.1.${'$'}JB_SPACE_EXECUTION_NUMBER"
             args["HTTP_PROXY"] = "http://10.20.30.1:123"
         }
 
         push("bermos.registry.jetbrains.space/p/claerance/containers/claerance") {
-            tags("0.1")
+            tags("0.1.\$JB_SPACE_EXECUTION_NUMBER")
         }
     }
 }

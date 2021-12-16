0d999dce7cf63e7036f0cbe864e2787eaccc68d4
Matthew Machivenyika <matthew.machivenyika@fis-he.ch>
2021-12-16 07:12:20 +0100

    Minor build script updates

--- .space.kts
+++ .space.kts
@@ -2,7 +2,7 @@ job("Build and push Docker") {
      startOn {
         gitPush { 
         	branchFilter {
-                +"refs/heads/master"
+                +"refs/heads/main"
             }
         }
      }
@@ -12,11 +12,12 @@ job("Build and push Docker") {
             context = "."
             file = "./docker/Dockerfile"
             labels["vendor"] = "Bermos Inc."
+            labels["version"] = "0.1"
             args["HTTP_PROXY"] = "http://10.20.30.1:123"
         }
 
         push("bermos.registry.jetbrains.space/p/claerance/containers/claerance") {
-            tags("version1.0")
+            tags("0.1")
         }
     }
 }
